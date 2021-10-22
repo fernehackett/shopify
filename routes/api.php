86ca207013ad4a75f05e005721a2798b3389a2a2
Nic <tmninh94@gmail.com>
2021-10-22 09:57:34 +0700

    "Nic"

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::any("webHook", 'StoreController@webHook')->name("webHook");
+Route::any("install", 'StoreController@install');
