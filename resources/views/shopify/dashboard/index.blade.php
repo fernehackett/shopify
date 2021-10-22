db6f0517edc8106532c278815f8b8a25ee4aa787
Nic <tmninh94@gmail.com>
2021-10-22 09:04:02 +0700

    "Nic"

--- resources/views/shopify/dashboard/index.blade.php
+++ resources/views/shopify/dashboard/index.blade.php
@@ -1,8 +1,22 @@
 @extends('shopify.default')
 
 @section('page-header')
-    Shopify {{ $store->name }}
+    Hello, {{ $store->name }}
 @stop
 @section('content')
-{{--    @dump($store)--}}
+    <div class="bgc-white bd bdrs-3 p-20 mB-20">
+        {{ Form::open([
+            "url" => route("shopify.anti-theft"),
+            "method" => "POST"
+        ]) }}
+            <div class="form-check">
+                <label class="form-label form-check-label">
+                    @isset($antiTheft)
+                        <input type="hidden" name="script_id" value="{{ $antiTheft->script_id }}">
+                    @endisset
+                    <input onchange="this.form.submit()" class="form-check-input" name="anti-theft" value="on" type="checkbox" @isset($antiTheft) checked @endisset> Anti Theft
+                </label>
+            </div>
+        {{ Form::close() }}
+    </div>
 @stop
