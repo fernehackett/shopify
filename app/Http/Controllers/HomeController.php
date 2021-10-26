ec47c07bc6499361c4866d9c25eff79bd6b1c737
Nic <tmninh94@gmail.com>
2021-10-26 09:24:51 +0700

    "Nic"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -51,7 +51,7 @@ class HomeController extends Controller
                 'SharedSecret' => $api_secret,
             ];
             $redirectUrl = route("success");
-            new ShopifySDK($config);
+            $sp = new ShopifySDK($config);
             $authUrl = \PHPShopify\AuthHelper::createAuthRequest($scope, $redirectUrl, null, null, true);
             return redirect()->to($authUrl);
         }catch(\Exception $ex){
