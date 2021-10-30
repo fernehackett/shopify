4d8061d02ca6c8a8a683c7e93aec24a375bfbfd7
Nic <tmninh94@gmail.com>
2021-10-30 15:31:04 +0700

    "Nic"

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -10,45 +10,45 @@
 <div class="pos-a t-0 l-0 bgc-white w-100 h-100 d-f fxd-r fxw-w ai-c jc-c pos-r p-30">
     <div class="col-md-8 col-md-offset-2 page-content-wrap">
         <h2>Privacy Policy</h2>
-        <p>This website (the “Site”) is operated by Architechpro OÜ (“Architechpro”, “we”, “us” and/or “our”) and has
+        <p>This website (the “Site”) is operated by {{ env("APP_NAME") }} (“{{ env("APP_NAME") }}”, “we”, “us” and/or “our”) and has
             been created to provide our Site visitors (“you”, “your”) with information about our company and our e-mail
-            collection tools. This Privacy Policy (this “Policy”) sets forth Architechpro’s policy with respect to
+            collection tools. This Privacy Policy (this “Policy”) sets forth {{ env("APP_NAME") }}’s policy with respect to
             information, including personally identifiable data (such personally identifiable data “Personal
             Information”), that is collected from you through the Site or through our other service offerings
             (collectively, including the Site, the “Services”). Certain Services may be made available to you on
             websites owned by our end customers (“End Customers”), and the information collected via these embedded
-            widgets (“Tools”) is directed to Architechpro OÜ and governed by this Policy.</p>
+            widgets (“Tools”) is directed to {{ env("APP_NAME") }} and governed by this Policy.</p>
         <p>The Services and our business may change from time to time. As a result, at times it may be necessary for
-            Architechpro OÜ to make changes to this Policy. Architechpro&nbsp;OÜ reserves the right to update this
+            {{ env("APP_NAME") }} to make changes to this Policy. {{ env("APP_NAME") }} reserves the right to update this
             Policy at any time and from time to time without prior notice. Please review this Policy periodically, and
             especially before you provide any Personal Information. This Policy was last updated on the date indicated
             above. Your continued use of the Services after any changes or revisions to this Policy shall indicate your
             agreement with the terms of such revised Policy.</p>
-        <p>1. Information Collected<br> When you interact with us through the Site or other Services, we may collect
+        <p><b>1. Information Collected</b><br> When you interact with us through the Site or other Services, we may collect
             Personal Information and other information from you, as further described below:</p>
         <p>Personal Information That You Provide:<br> We collect Personal Information from you when you voluntarily
-            provide such information, such as when you contact us with inquiries, subscribe to our or a Architechpro End
+            provide such information, such as when you contact us with inquiries, subscribe to our or a {{ env("APP_NAME") }} End
             Customer’s e-mail newsletter, register for access to the Services or use certain Services. For instance,
-            Architechpro may collect the following information about you: name, address, zip code, telephone number and
+            {{ env("APP_NAME") }} may collect the following information about you: name, address, zip code, telephone number and
             e-mail address. The information collected may vary. For example, our End Customers may customize the data
             that is requested via the Tools and may collect other information, such as your favourite color or
-            birthdate. Wherever Architechpro&nbsp;OÜ collects Personal Information we make an effort to provide a link
+            birthdate. Wherever {{ env("APP_NAME") }} collects Personal Information we make an effort to provide a link
             to this Policy.</p>
         <p>By voluntarily providing us with Personal Information, you are consenting to our use of it in accordance with
             this Policy. If you provide Personal Information to the Site or through the other Services, you acknowledge,
             agree, and expressly consent that such Personal Information may be transferred from your current location to
-            the offices and servers of Architechpro OÜ and the authorized third parties referred to herein located in
+            the offices and servers of {{ env("APP_NAME") }} and the authorized third parties referred to herein located in
             Estonia, Ukraine and the United States.</p>
-        <p>Other Information:<br> Non-Identifiable Data: When you interact with Architechpro through the Services, we
+        <p><b>Other Information:</b><br> Non-Identifiable Data: When you interact with {{ env("APP_NAME") }} through the Services, we
             receive and store certain personally non-identifiable information. Such information, which is collected
             passively using various technologies, cannot presently be used to specifically identify you, unless it is
-            combined with Personal Information. Architechpro may store such information itself or such information may
-            be included in databases owned and maintained by Architechpro affiliates, agents or service providers. We
+            combined with Personal Information. {{ env("APP_NAME") }} may store such information itself or such information may
+            be included in databases owned and maintained by {{ env("APP_NAME") }} affiliates, agents or service providers. We
             may use such information and pool it with other information to track, for example, the total number of
             visitors to our Site, the number of visitors to each page of our Site, and the domain names of our visitors’
-            Internet service providers. It is important to note that Architechpro OÜ uses no Personal Information in
+            Internet service providers. It is important to note that {{ env("APP_NAME") }} uses no Personal Information in
             this process.</p>
-        <p>Cookies: In providing the Services, we may use a technology called “cookies.” A cookie is a piece of
+        <p><b>Cookies:</b><br> In providing the Services, we may use a technology called “cookies.” A cookie is a piece of
             information that the computer that hosts our Site gives to your browser when you access the Site or the
             Tools. Our cookies help provide additional functionality to the Services and help us analyze the Services
             usage more accurately. Certain information collected through the use of cookies may be used to serve you
@@ -72,7 +72,7 @@
             including Firefox 5+, Internet Explorer 9+, Safari 5.1+, and Google Chrome. Please consult //donottrack.us
             on how to enable this privacy setting on your browser. You can also change the way your browser handles
             cookies. Already existing cookies can be deleted. Please consult www.allaboutcookies.org on how to manage or
-            delete cookies in your browser. This Policy covers the use of cookies and similar files by Architechpro OÜ
+            delete cookies in your browser. This Policy covers the use of cookies and similar files by {{ env("APP_NAME") }}
             only and does not cover the use of cookies, flash cookies, and similar files by any third-party advertisers
             or websites linked to the Site. We also may use Google Analytics and other similar services. Google
             Analytics uses cookies to help analyze how users use the Site. The information generated by the cookie about
@@ -82,14 +82,14 @@
             and internet usage. Google may also transfer this information to third parties where required to do so by
             law, or where such third parties process the information on Google’s behalf. Google will not associate your
             IP address with any other data held by Google.</p>
-        <p>Aggregated Personal Information: In an ongoing effort to better understand and serve the users of the
-            Services, Architechpro OÜ often conducts research on its customer demographics, interests and behavior based
+        <p><b>Aggregated Personal Information:</b> In an ongoing effort to better understand and serve the users of the
+            Services, {{ env("APP_NAME") }} often conducts research on its customer demographics, interests and behavior based
             on the Personal Information and other information provided to us. This research may be compiled and analyzed
-            on an aggregate basis, and Architechpro OÜ may share this aggregate data with its affiliates, agents and
-            business partners. This aggregate information does not identify you personally. Architechpro may also
+            on an aggregate basis, and {{ env("APP_NAME") }} may share this aggregate data with its affiliates, agents and
+            business partners. This aggregate information does not identify you personally. {{ env("APP_NAME") }} may also
             disclose aggregated user statistics in order to describe our services to current and prospective business
             partners, and to other third parties for other lawful purposes.</p>
-        <p>2. Use of Information:<br> Architechpro uses the Personal Information you provide in a manner that is
+        <p><b>2. Use of Information:</b><br> {{ env("APP_NAME") }} uses the Personal Information you provide in a manner that is
             consistent with this Policy. If you provide Personal Information for a certain reason, we may use the
             Personal Information in connection with the reason for which it was provided. For instance, if you contact
             us by e-mail, we will use the Personal Information you provide to answer your question or resolve your
@@ -98,66 +98,66 @@
             marketing and promotional efforts and customize the Services content, layout, and Services to better suit
             your needs; and (c) resolve disputes, troubleshoot problems and enforce our Service Terms (as defined
             below).</p>
-        <p>In addition, Architechpro&nbsp;OÜ and its affiliates may use your information to contact you in the future to
+        <p>In addition, {{ env("APP_NAME") }} and its affiliates may use your information to contact you in the future to
             tell you about services we believe will be of interest to you. If we do so, each communication we send you
             will contain instructions permitting you to “opt-out” of receiving future communications. In addition, if at
             any time you wish not to receive any future communications or you wish to have your name deleted from our
             mailing lists, please contact us as indicated.</p>
-        <p>3. Disclosure of Information<br> There are certain circumstances in which we may share your Personal
+        <p><b>3. Disclosure of Information</b><br> There are certain circumstances in which we may share your Personal
             Information or other information with third parties without further notice to you, as set forth below:</p>
-        <p>End Customers: If you enter your Personal Information in any Tool on an End Customer’s website, we may share
+        <p><b>End Customers:</b> If you enter your Personal Information in any Tool on an End Customer’s website, we may share
             such Personal Information with the applicable End Customer and such End Customer’s service providers, as
             requested by such End Customer.</p>
-        <p>Business Transfers: As we develop our business, we might sell or buy businesses or assets. In the event of a
+        <p><b>Business Transfers:</b> As we develop our business, we might sell or buy businesses or assets. In the event of a
             corporate sale, merger, reorganization, dissolution or similar event, Personal Information may be part of
             the transferred assets.</p>
-        <p>Related Companies: We may also share your Personal Information with our affiliates for purposes consistent
+        <p><b>Related Companies:</b> We may also share your Personal Information with our affiliates for purposes consistent
             with this Policy.</p>
-        <p>Agents, Consultants and Related Third Parties: Architechpro OÜ, like many businesses, sometimes hires other
+        <p><b>Agents, Consultants and Related Third Parties:</b> {{ env("APP_NAME") }}, like many businesses, sometimes hires other
             companies or individuals to perform certain business-related functions. When we employ another company to
             perform a function of this nature, we only provide them with the information that they need to perform their
             specific function.</p>
-        <p>Legal Requirements: Architechpro&nbsp;OÜ may disclose your Personal Information if required to do so by law
+        <p><b>Legal Requirements:</b> {{ env("APP_NAME") }} may disclose your Personal Information if required to do so by law
             or in the good faith belief that such action is necessary to (a) comply with a legal obligation, including
             lawful requests by public authorities, including to meet national security or law enforcement requirements,
-            (b) protect and defend the rights or property of Architechpro OÜ, (c) act in urgent circumstances to protect
+            (b) protect and defend the rights or property of {{ env("APP_NAME") }}, (c) act in urgent circumstances to protect
             the personal safety of users of the Services or the public.</p>
-        <p>4. Security<br> Architechpro&nbsp;OÜ takes reasonable steps to protect the Personal Information provided via
+        <p><b>4. Security</b><br> {{ env("APP_NAME") }} takes reasonable steps to protect the Personal Information provided via
             the Services from loss, misuse, and unauthorized access, disclosure, alteration, or destruction. However, no
             Internet or e-mail transmission is ever fully secure or error free. In particular, e-mail sent to or from
             the Site may not be secure. Therefore, you should take special care in deciding what information you send to
-            us via e-mail. Please keep this in mind when disclosing any Personal Information to Architechpro&nbsp;OÜ via
+            us via e-mail. Please keep this in mind when disclosing any Personal Information to {{ env("APP_NAME") }} via
             the Internet.</p>
-        <p>5. Third Party Sites<br> This Policy applies only to the Services. The Site and other Services, including the
-            Tools, may contain links to other websites not operated or controlled by Architechpro OÜ, including those
+        <p><b>5. Third Party Sites</b><br> This Policy applies only to the Services. The Site and other Services, including the
+            Tools, may contain links to other websites not operated or controlled by {{ env("APP_NAME") }}, including those
             operated by End Customers (the “Third Party Sites”). The policies and procedures described here do not apply
             to the Third Party Sites or services, or to the use of information submitted via a Tool on such End
             Customer’s website by the applicable End Customer or its service providers. The links from the Site or
-            placement of Tools on certain Third Party Sites does not imply that Architechpro&nbsp;OÜ endorses or has
+            placement of Tools on certain Third Party Sites does not imply that {{ env("APP_NAME") }} endorses or has
             reviewed the Third Party Sites. We suggest contacting those sites, services, entities or persons directly
             for information on their privacy policies.</p>
-        <p>6. Children’s Online Privacy Protection<br> Architechpro&nbsp;OÜ does not knowingly collect Personal
+        <p><b>6. Children’s Online Privacy Protection</b><br> {{ env("APP_NAME") }} does not knowingly collect Personal
             Information from children under the age of 13. If you are under the age of 13, please do not submit any
             Personal Information through the Services. We encourage parents and legal guardians to monitor their
             children’s Internet usage and to help enforce our Policy by instructing their children never to provide
             Personal Information through the Services without their permission. If you have reason to believe that a
-            child under the age of 13 has provided Personal Information to Architechpro OÜ, please contact us, and we
+            child under the age of 13 has provided Personal Information to {{ env("APP_NAME") }}, please contact us, and we
             will endeavor to delete that information from our databases.</p>
-        <p>7. Exclusions<br> This Policy does not apply to any Personal Information collected by Architechpro OÜ other
+        <p><b>7. Exclusions</b><br> This Policy does not apply to any Personal Information collected by {{ env("APP_NAME") }} other
             than Personal Information collected through the Services. This Policy shall not apply to any unsolicited
-            information you provide to Architechpro&nbsp;OÜ through the Services or through any other means. This
+            information you provide to {{ env("APP_NAME") }} through the Services or through any other means. This
             includes any ideas for new products or modifications to existing products, and other unsolicited submissions
             (collectively, “Unsolicited Information”). All Unsolicited Information shall be deemed to be
-            non-confidential and Architechpro OÜ shall be free to reproduce, use, disclose, and distribute such
+            non-confidential and {{ env("APP_NAME") }} shall be free to reproduce, use, disclose, and distribute such
             Unsolicited Information to others without limitation or attribution.</p>
-        <p>8. Access to Information; Contacting Architechpro&nbsp;OÜ<br> To keep your Personal Information accurate,
+        <p><b>8. Access to Information; Contacting {{ env("APP_NAME") }}</b><br> To keep your Personal Information accurate,
             current, and complete, please contact us as specified below. We will take reasonable steps to update or
             correct Personal Information in our possession that you have previously submitted via the Services. Please
             also feel free to contact us if you have any questions about this Policy or the information practices of
-            this Site. You may contact us as follows: architechpro.contact@gmail.com.</p>
-        <p>9. EU Personal Data<br> With respect to the processing of personal data relating to data subjects located in
+            this Site. You may contact us as follows: {{ env("APP_NAME") }}.contact@gmail.com.</p>
+        <p><b>9. EU Personal Data</b><br> With respect to the processing of personal data relating to data subjects located in
             the European Economic Area (including the United Kingdom as of the Effective Date of this Privacy Policy) by
-            Architechpro OÜ solely on behalf of the End Customer, the terms of the Data Processing Addendum shall apply.
+            {{ env("APP_NAME") }} solely on behalf of the End Customer, the terms of the Data Processing Addendum shall apply.
             The following terms have the meanings given in the General Data Protection Regulation (EU) 2016/679
             (“GDPR”): “personal data”, “data subject” and “process”.</p>
     </div>
