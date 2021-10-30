7866a53fbd7c1e77da9d26ec6c229c197d886b11
Nic <tmninh94@gmail.com>
2021-10-30 11:49:56 +0700

    "Nic"

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        return User::updateOrCreate(['name' => $data['name']],[
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
