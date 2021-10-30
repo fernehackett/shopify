11c81fe31411de95bf98ea602e8b421dbd11d6d8
Nic <tmninh94@gmail.com>
2021-10-30 13:31:04 +0700

    "Nic"

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 Route::group(['middleware'=>["verify.shopify"]], function () {
     Route::get('/', "Shopify\DashboardController@index")->name("home");
-});
-Route::group(["as"=>"shopify.","namespace"=>"Shopify",'middleware'=>["auth"]], function(){
-    Route::post("anti-theft", 'DashboardController@antiTheft')->name("anti-theft");
+    Route::group(["as"=>"shopify.","namespace"=>"Shopify"], function(){
+        Route::post("anti-theft", 'DashboardController@antiTheft')->name("anti-theft");
+    });
 });
 
 Route::get('policy', 'HomeController@policy')->name("policy");
