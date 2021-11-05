86b921e0db16a9517cbed7d7da42d5670915b974
Nic <tmninh94@gmail.com>
2021-11-05 14:09:43 +0700

    "Nic"

--- database/seeders/create_plan_data.php
+++ database/seeders/create_plan_data.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Osiset\ShopifyApp\Storage\Models\Plan;
 
 class create_plan_data extends Seeder
@@ -14,7 +15,9 @@ class create_plan_data extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Plan::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         Plan::create([
            "type" => "RECURRING",
             "name" => "Monthly",
