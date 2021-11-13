39e9e236439a2fd8071058e994a879476e3316de
Nic <tmninh94@gmail.com>
2021-11-13 09:53:37 +0700

    "Nic"

--- app/Http/Controllers/Shopify/DashboardController.php
+++ app/Http/Controllers/Shopify/DashboardController.php
@@ -44,10 +44,10 @@ class DashboardController extends Controller
                 $scriptTag["script_id"] = $scriptTag["id"];
                 unset($scriptTag["id"]);
                 ScriptTag::create($scriptTag);
-                return back();
+                return redirect(route("home",["message"=>"Enable successfully!"]));
             } catch (\Exception $e) {
                 \Log::error($e->getMessage());
-                return back();
+                return redirect(route("home",["message"=>"Enable Failed!"]));
             }
         } else {
             $script_id = $request->get("script_id");
@@ -57,7 +57,7 @@ class DashboardController extends Controller
                 \Log::error($ex->getMessage());
             }
             ScriptTag::where("script_id", $script_id)->delete();
-            return back();
+            return redirect(route("home",["message"=>"Disable successfully!"]));
         }
     }
 
