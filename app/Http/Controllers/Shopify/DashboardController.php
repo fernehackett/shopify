3c9989dc56434134f065eba19d3860a7f6bd6eab
fernehackett <92840634+fernehackett@users.noreply.github.com>
2021-11-16 17:06:01 +0700

    Update DashboardController.php

--- app/Http/Controllers/Shopify/DashboardController.php
+++ app/Http/Controllers/Shopify/DashboardController.php
@@ -12,9 +12,6 @@ class DashboardController extends Controller
 
     public function __construct()
     {
-        $this->api_key = env("SHOPIFY_API_KEY", "9980cee5ec9b979c47ef231b98d4330a");
-        $this->api_secret = env("SHOPIFY_SECRET", "84d73168d39f6b575407c3b3e8249ae2");
-        $this->scope = env("SHOPIFY_SCOPE", "read_products,read_orders,write_orders,write_script_tags,read_script_tags,write_products");
     }
 
     public function index(Request $request)
