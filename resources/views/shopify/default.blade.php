a528c9b9daaa43d5af3acf258ae628f5eb8652ca
Nic <tmninh94@gmail.com>
2021-11-19 21:32:08 +0700

    "Nic"

--- resources/views/shopify/default.blade.php
+++ resources/views/shopify/default.blade.php
@@ -3,9 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="hmac" content="{{ session("hmac") }}">
-    <meta name="store_url" content="{{ session("store_url") }}">
-    <meta name="calculated_hmac" content="{{ session("calculated_hmac") }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
